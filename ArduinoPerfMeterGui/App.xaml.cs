b75f0b2089d38df777325f0d3f0d16ba21fadf41
Raymond <raytsui@hotmail.com>
2016-06-08 16:42:26 +0800

    Baseline

---
+++ ArduinoPerfMeterGui/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace ArduinoPerfMeterGui
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    ///
+    public partial class App : Application
+    {
+    }
+}
