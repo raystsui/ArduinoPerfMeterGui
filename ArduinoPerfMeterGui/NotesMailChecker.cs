1f7489b6e852d5961ee48fe83282b5379e32f640
Raymond <raytsui@hotmail.com>
2016-06-17 13:54:04 +0800

    C006

--- ArduinoPerfMeterGui/NotesMailChecker.cs
+++ ArduinoPerfMeterGui/NotesMailChecker.cs
@@ -158,7 +158,7 @@ namespace ArduinoPerfMeterGui
             }
             else
             {
-                programState = ProgramState.Disconnected;
+                CloseNotesDatabase(hDb);
                 return false;
             }
         }
